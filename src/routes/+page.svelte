43b493ea4ac48eb12db7ea8a650bd3c38d04e499
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-06-25 21:58:02 +0200

    ActiviteBlock + images

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -148,8 +148,8 @@
 		{
 			type: 'AProposBlock',
 			props: {
-				title: 'Une association de quartier à Laeken, active depuis 1985.',
-				polygon: '/assets/svg/Polygon.svg'
+				polygon: '/assets/svg/Polygon.svg',
+				title: 'Une association de quartier à Laeken, active depuis 1985.'
 			}
 		},
 		{
@@ -253,6 +253,159 @@
 						title: 'Stagiaires',
 						description:
 							'Nous accueillons chaque année des étudiants en assistant social et éducateur spécialisé pour leur première expérience.'
+					}
+				]
+			}
+		},
+		{
+			type: 'ActivitéBlock',
+			props: {
+				polygon: '/assets/svg/Polygon.svg',
+				title: 'Des Formation et de L’aide, pensées pour vous.'
+			}
+		},
+		{
+			type: 'DiverseBlock',
+			props: {
+				title: 'Ce que nous proposons',
+				activities: [
+					{
+						id: 1,
+						title: 'Apprendre le français',
+						description:
+							'Atteignez le niveau A2 pour gagner en confiance dans votre vie quotidienne et professionnelle. Un apprentissage adapté à votre rythme.',
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/asbl2.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/asbl20.png',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/asbl8.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
+					},
+					{
+						id: 2,
+						title: 'Parcours d’accueil',
+						description:
+							'Accompagnement complet pour les nouveaux arrivants (COCOF). 16 à 20h par semaine dédiées au français, aux démarches et aux liens sociaux.',
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/asbl12.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/asbl10.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/asbl19.png',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
+					},
+					{
+						id: 3,
+						title: 'L’école des devoirs',
+						description:
+							'De la 1ère à la 6ème primaire. Un cadre calme pour la préparation du CEB et des activités culturelles épanouissantes.',
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/asbl7.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/asbl17.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/asbl3.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
+					},
+					{
+						id: 4,
+						title: 'Ateliers numériques',
+						description:
+							'Apprenez à maîtriser Internet et les outils essentiels : Itsme, Actiris, e-mails, Word et vos démarches en ligne en toute autonomie.',
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/asbl6.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/asbl5.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/permanence_informatique_3.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
+					},
+					{
+						id: 5,
+						title: 'Permance sociale',
+						description:
+							"Besoin d'aide pour le logement, une inscription scolaire, le CPAS ou une administration ? Un accueil confidentiel et respectueux.",
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/permanence_sociale_1.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/permanence_sociale_2.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/permanence_sociale_3.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
+					},
+					{
+						id: 6,
+						title: 'Vivre ensemble',
+						description:
+							"Parce que l'intégration passe aussi par le plaisir et la rencontre: sorties culturelles, fêtes de quartier et ateliers de débat citoyen.",
+						images: [
+							{
+								id: 1,
+								src: '/assets/images/asbl9.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 2,
+								src: '/assets/images/asbl1.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							},
+							{
+								id: 3,
+								src: '/assets/images/asbl18.jpeg',
+								alt: 'Cours de français à Entraide Bruxelles'
+							}
+						]
 					}
 				]
 			}
