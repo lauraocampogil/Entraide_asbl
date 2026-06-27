1bf3270ae246ff0b06053cf053fd714610c75e65
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-06-27 23:16:10 +0200

    svelte.config for lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import '../app.css';
+	import './layout.css';
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
 
