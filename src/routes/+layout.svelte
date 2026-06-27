9b4f68d76417369d2f15cdb0ab93c3867c5095df
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-06-28 00:34:03 +0200

    Changed layout to app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import './layout.css';
+	import '../app.css';
 	import Header from '$lib/components/layout/Header.svelte';
 	import Footer from '$lib/components/layout/Footer.svelte';
 
