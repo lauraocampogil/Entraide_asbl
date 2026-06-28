40880a3535ca41b6b35f273ffa1b326e95879fc5
lauraocampogil <146712561+lauraocampogil@users.noreply.github.com>
2026-06-28 10:39:57 +0200

    Deleted the language button

--- src/lib/components/layout/Header.svelte
+++ src/lib/components/layout/Header.svelte
@@ -39,29 +39,6 @@
 			{/each}
 		</nav>
 
-		<!-- FR + (décoratif) desktop -->
-		<div
-			class="hidden xl:flex items-center gap-1 font-fustat text-body text-dark cursor-default select-none"
-		>
-			FR
-			<svg
-				width="10"
-				height="6"
-				viewBox="0 0 10 6"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				aria-hidden="true"
-			>
-				<path
-					d="M1 1L5 5L9 1"
-					stroke="currentColor"
-					stroke-width="1.5"
-					stroke-linecap="round"
-					stroke-linejoin="round"
-				/>
-			</svg>
-		</div>
-
 		<!-- Burger mobile -->
 		<button
 			type="button"
